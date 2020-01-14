c69385efce198351bf86deb4e2a82288d392248a
Sam Randels <samrandels@gmail.com>
2020-01-13 21:45:18 -0500

    Add current condition display

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,17 +5,22 @@ GLOBAL VARIABLES
 let currentDate;
 let currentCity = "Washington DC";
 let currentTemp;
+let currentMaxTemp;
 let currentUV;
 let currentHumidity;
 let currentWindSpeed;
 let currentLat = "";
 let currentLon = "";
+let forecastMaxTemp = [];
+let forecastMinTemp = [];
 
 $(document).ready(function() {
   let weatherURL = "https://api.openweathermap.org/data/2.5/weather?";
   let uvURL = "https://api.openweathermap.org/data/2.5/uvi?";
+  let forecastURL = "https://api.openweathermap.org/data/2.5/forecast?";
 
   setTime();
+  displayCity();
   // AJAX CALL TO OPENWEATHERMAP
 
   $.ajax({
@@ -29,20 +34,21 @@ $(document).ready(function() {
     success: function(response) {
       console.log(response);
       currentTemp = response.main.temp;
+      currentMaxTemp = response.main.temp_max;
       currentHumidity = response.main.humidity;
       currentWindSpeed = response.wind.speed;
       currentLat = response.coord.lat;
       currentLon = response.coord.lon;
-      console.log(currentLat);
       getUV();
+      displayCurrentConditions();
+      getForecast();
     },
     error: function() {
-      console.log("ERROR");
+      console.log("ERROR CURRENT WEATHER");
     }
   });
 
-
-//   MAKES SEPARATE CALL TO OPENWEATHER API TO FETCH UV INDEX
+  //   MAKES SEPARATE CALL TO OPENWEATHER API TO FETCH UV INDEX
   function getUV() {
     $.ajax({
       url: uvURL,
@@ -51,18 +57,82 @@ $(document).ready(function() {
         lat: currentLat,
         lon: currentLon,
         APPID: "968b7fae60d97ecc132a2221d027b935"
+      },
+      success: function(response) {
+        currentUV = response.value;
+        console.log(currentUV);
+        displayUV();
+      },
+      error: function() {
+        console.log("ERROR UV");
       }
-    }).then(function(response) {
-      currentUV = response.value;
-      console.log(currentUV);
     });
-  };
-  
+  }
 
-//   FETCHES CURRENT TIME AS A MOMENT OBJECT OF GIVEN FORMAT
-  function setTime (){
+  //   MAKES SEPARATE CALL TO OPENWEATHER API TO FETCH 5 DAY FORECAST
+  //   PUSHES THOSE VALUES TO THE ARRAY
+  function getForecast() {
+    $.ajax({
+      url: forecastURL,
+      method: "GET",
+      data: {
+        q: currentCity,
+        APPID: "968b7fae60d97ecc132a2221d027b935",
+        cnt: "5",
+        units: "imperial"
+      },
+      success: function(response) {
+        $.each(response.list, function(idx, val) {
+          forecastMaxTemp.push(val.main.temp_max);
+          forecastMinTemp.push(val.main.temp_min);
+        });
+      },
+      error: function() {
+        console.log("ERROR FORECAST");
+      }
+    });
+  }
+
+  //   FETCHES CURRENT TIME AS A MOMENT OBJECT OF GIVEN FORMAT
+  function setTime() {
     currentDate = moment().format("dddd, MMMM Do, YYYY");
     console.log(currentDate);
   }
 
+  //   CALLS ALL THE DISPLAY FUNCTIONS FOR CURRENT CONDITIONS
+  function displayCurrentConditions() {
+    displayUV();
+    displayCurrentTemp();
+    displayWindSpeed();
+    displayCurrentHigh();
+    displayHumidity();
+  }
+
+  //   DISPLAYS CURRENT CITY IN UPPERCASE
+  function displayCity() {
+    uppercaseCity = currentCity.toUpperCase();
+    $("#city").text(uppercaseCity);
+  }
+
+  //   DISPLAYS CURRENT UV
+  function displayUV() {
+    $("#uvIndex").text("UV INDEX: " + currentUV);
+  }
+
+  //   DISPLAYS CURRENT HIGH
+  function displayCurrentHigh() {
+    $("#highTemp").text("TODAY'S HIGH: " + currentMaxTemp + "\u00B0");
+  }
+  //   DISPLAYS CURRENT TEMP
+  function displayCurrentTemp() {
+    $("#currentTemp").text("CURRENT TEMP: " + currentTemp + "\u00B0");
+  }
+  // DISPLAYS CURRENT WIND SPEED
+  function displayWindSpeed() {
+    $("#wind").text("WIND SPEED: " + currentWindSpeed + " MPH");
+  }
+  // DISPLAYS CURRENT HUMIDITY
+  function displayHumidity() {
+    $("#humidity").text("HUMIDITY: " + currentHumidity + "%");
+  }
 });
